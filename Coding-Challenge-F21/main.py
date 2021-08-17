914e1e7fd9cbbc9bf5d570b28013efb583db08f7
Joseph Barsis <33675219+joe2475@users.noreply.github.com>
2021-08-17 11:55:22 -0500

    Add files via upload

---
+++ Coding-Challenge-F21/main.py
@@ -0,0 +1,35 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyzer = SentimentIntensityAnalyzer()
+text = ""
+sentences = []
+postive_score = 0.0
+negative_score = 0.0
+neutral_score = 0.0
+total_count = 0.0
+
+
+
+with open("input.txt","r") as f:
+    for line in f.read().split('\n'):
+        text = analyzer.polarity_scores(line)
+        sentences.append(text)
+        if text['compound'] >= .05:
+            postive_score += 1
+        elif text['compound']  <= -.05:
+            negative_score +=1
+        else:
+            neutral_score +=1
+        total_count +=1
+    f.close()
+
+with open("sentenceSentiment.txt", "w") as g:
+    for sentence in sentences:
+        g.write("%s\n" % sentence)
+    g.close()
+
+print("Total positve score is {}%".format(postive_score/total_count*100.0))
+print("Total negative score is {}%".format(negative_score/total_count*100.0))
+print("Total neutral score is {}%".format(neutral_score/total_count*100.0))
+        
+
+
